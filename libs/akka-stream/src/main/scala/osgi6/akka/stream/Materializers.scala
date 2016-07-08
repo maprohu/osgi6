3d0c14f738960fc793458085ff2fc8b9e5d0ee11
maprohu <maprohu@gmail.com>
2016-07-08 17:05:50 +0100

    preapre release

---
+++ libs/akka-stream/src/main/scala/osgi6/akka/stream/Materializers.scala
@@ -0,0 +1,31 @@
+package osgi6.akka.stream
+
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings, Supervision}
+
+import scala.util.control.NonFatal
+
+/**
+  * Created by pappmar on 08/07/2016.
+  */
+object Materializers {
+
+  def retry(implicit
+    actorSystem: ActorSystem
+  ) = {
+    val decider : Supervision.Decider = {
+      case NonFatal(ex) =>
+        actorSystem.log.error(ex, "error in stream processing")
+        Supervision.Resume
+      case ex =>
+        actorSystem.log.error(ex, "fatal error in stream processing")
+        Supervision.Stop
+    }
+
+    ActorMaterializer(
+      ActorMaterializerSettings(actorSystem)
+        .withSupervisionStrategy(decider)
+    )
+  }
+
+}
