08dff909a7351d017f833b9be3df68ce52550cbb
maprohu <maprohu@gmail.com>
2016-07-09 17:08:04 +0100

    unloading

--- bundles/multi/bundle/src/main/scala/osgi6/multi/MultiActivator.scala
+++ bundles/multi/bundle/src/main/scala/osgi6/multi/MultiActivator.scala
@@ -6,6 +6,7 @@ import osgi6.common.BaseActivator
 /**
   * Created by martonpapp on 04/07/16.
   */
+@deprecated("do not use global executioncontext in osgi - fails to unload")
 class MultiActivator extends BaseActivator({ ctx =>
 
   import scala.concurrent.ExecutionContext.Implicits.global
