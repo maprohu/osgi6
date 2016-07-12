d159a2f071b8c77be62b0b45c6c50cfd9b70f6c5
maprohu <maprohu@gmail.com>
2016-07-12 22:58:24 +0100

    ftx

--- bundles/multi/bundle/src/main/scala/osgi6/multi/MultiActivator.scala
+++ bundles/multi/bundle/src/main/scala/osgi6/multi/MultiActivator.scala
@@ -1,7 +1,14 @@
 package osgi6.multi
 
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+
 import osgi6.api.OsgiApi
 import osgi6.common.{BaseActivator, HygienicThread}
+import osgi6.lib.multi.MultiProcessor
+import osgi6.multi.api.MultiApi
+
+import scala.concurrent.{Await, ExecutionContext, Future, Promise}
+import scala.concurrent.duration._
 
 /**
   * Created by martonpapp on 04/07/16.
@@ -13,7 +20,7 @@ class MultiActivator extends BaseActivator({ ctx =>
 
   implicit val (ec, pool) = HygienicThread.createExecutionContext
 
-  val proc = MultiProcessor.apply
+  val proc = MultiActivator.osgiHandler
 
   val reg = OsgiApi.registry.register(proc)
 
@@ -23,3 +30,26 @@ class MultiActivator extends BaseActivator({ ctx =>
   }
 
 })
+object MultiActivator {
+
+  def osgiHandler(implicit executionContext: ExecutionContext) : OsgiApi.Handler = {
+    new OsgiApi.Handler {
+      override def process(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+
+        val processed = Await.result(
+          MultiProcessor.process(request, response),
+          1.minute
+        )
+
+        if (!processed) {
+          response.setStatus(HttpServletResponse.SC_NOT_FOUND)
+        }
+
+      }
+    }
+
+  }
+
+
+
+}
---
+++ libs/multi/src/main/scala/osgi6/lib/multi/MultiProcessor.scala
@@ -0,0 +1,48 @@
+package osgi6.lib.multi
+
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+
+import osgi6.multi.api.MultiApi
+
+import scala.concurrent.{Await, Future, Promise}
+import scala.concurrent.duration._
+
+/**
+  * Created by martonpapp on 12/07/16.
+  */
+object MultiProcessor {
+
+  def process(request: HttpServletRequest, response: HttpServletResponse): Future[Boolean] = {
+    import scala.collection.JavaConversions._
+
+    val promise = Promise[Boolean]()
+
+    val handlers: Iterator[MultiApi.Handler] = MultiApi.registry.iterate
+
+    def processNext : Unit = {
+      if (handlers.hasNext) {
+        val handler = handlers.next()
+
+        handler.dispatch(request, response, new MultiApi.Callback {
+          override def handled(result: Boolean): Unit = {
+            if (result) {
+              promise.success(true)
+            } else {
+              processNext
+            }
+          }
+        })
+
+      } else {
+        promise.success(false)
+      }
+
+    }
+
+    processNext
+
+    promise.future
+
+  }
+
+}
